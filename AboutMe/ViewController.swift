b06da130ccebd254dcd96071e094c5761a030ea8
Fredrick Muikia <ftmrk9@mail.umsl.edu>
2020-06-17 02:14:20 -0500

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by CompSci 4220 Student on 6/17/20.
+//  Copyright © 2020 umsl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
