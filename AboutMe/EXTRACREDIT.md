824f2e060ee9b316874b36a14a9b149922ec5e48
ftmrk9 <ftmrk9@mail.umsl.edu>
2020-08-26 15:24:10 -0500

    first commit

---
+++ AboutMe/EXTRACREDIT.md
@@ -0,0 +1,20 @@
+{
+  "configurations" : [
+    {
+      "id" : "73453581-D255-435C-A9F6-366791581FA2",
+      "name" : "Configuration 1",
+      "options" : {
+
+      }
+    }
+  ],
+  "defaultOptions" : {
+
+  },
+  "testTargets" : [
+
+  ],
+  "version" : 1
+}
+
+For extra credit, I added two buttons, one that shows my profile picture and one that shows off my G35
--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var Name: UILabel!
+    @IBOutlet weak var Profile: UILabel!
+    
+    let image = UIImage(named: "Camera")
+    let g35 = UIImage(named: "G35")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = image
+        
+        imageView.layer.borderWidth = 1
+        imageView.layer.masksToBounds = false
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.layer.cornerRadius = 105
+        imageView.clipsToBounds = true
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let title = sender.title(for: .normal)
+        
+        if title == "ME" {
+            imageView.image = image
+        } else if title == "G35" {
+            imageView.image = g35
+        }
+    }
+    
 }
 
